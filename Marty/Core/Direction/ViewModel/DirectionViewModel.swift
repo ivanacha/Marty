379646753c3832a78c363d9b67ef9b130784e2b9
Ivan C. Acha <ivanacha4@gmail.com>
2025-10-15 19:21:58 -0400

    Add DirectionViewModel class
    Introduces DirectionViewModel as an ObservableObject to include all helper functions used in the main View.

---
+++ Marty/Core/Direction/ViewModel/DirectionViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  DirectionViewModel.swift
+//  Marty
+//
+//  Created by iVan on 10/15/25.
+//
+
+import Foundation
+
+import Combine
+
+@MainActor
+class DirectionViewModel: ObservableObject {
+    
+    // The ModelView should include all the functions executed in the main View.
+}
